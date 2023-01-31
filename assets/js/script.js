f5147c0e490a0dd20e2527b108217d9426cb7644
helenahagman <helena@rapibo.se>
2023-01-31 13:31:37 +0000

    Add some initial code to js file

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+
+
+let score = 0;
+function question = [
+    {
+        question: "New York City is composed of between 36 and 42 islands",
+        answer: true
+    },
+    {
+        question: "The Tour de France always finishes in Italy",
+        answer: false
+    },
+];
+
+if (response === "true" && question.answer === true) {
+    score++;
+    alert("Correct!");
+    } else if {
+    (response === "false" && question.answer === false) {
+    score++;
+    alert("Correct!");
+    } else {
+    alert("Incorrect.");    
+    }
+    } 
